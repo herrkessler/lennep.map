f8bd544244d67c2ec0f12d6fdd170ee3ac43acf8
Sebastian Kessler <sebastian@herrkessler.de>
2015-02-02 11:42:48 +0100

    tidy up scss and add google maps

---
+++ assets/js/map.js
@@ -0,0 +1,17 @@
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+}
+
+function loadScript() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyD6xOt7koXmNM3gaOll-yVyM4RTImT4LRY&sensor=true&callback=initialize";
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
