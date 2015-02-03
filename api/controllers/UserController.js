1cad309e8122c36a9eeab738d016a91c46be3e16
Sebastian Kessler <sebastian@herrkessler.de>
2015-02-03 14:44:59 +0100

    add basic user controller

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,37 @@
  */
 
 module.exports = {
+
+  // Index
+
+  index: function (req, res) {
+    User.find().populate('venuesList').exec(function(err, users) {
+      if (req.isSocket){
+        res.json({users: users});
+      }
+      else {
+        // var sessionUser = req.session.User.id;
+        res.view({
+          users: users
+        });
+      }
+    });
+  },
+  
+  // Show
+
+  show: function(req, res) {
+    Users.find().where({'id': req.param('id')}).populate('venuesList').exec(function(err, user) {
+      if (req.isSocket){
+        res.json({user: user});
+      }
+      else {
+        res.view({
+          user: user
+        });
+      }
+    });
+  }
 	
 };
 
